f18cc3555015f7a9d761bd2228cf9f4c50822587
Jeroen Nouws <info@moes-media.be>
2018-01-12 01:22:47 +0100

    changed the storybook font

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,11 +1,16 @@
 //@flow
 import React from 'react'
 
-import { storiesOf } from '@storybook/react'
+import { storiesOf, addDecorator } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo'
 import 'stories/Theme'
 import 'stories/Button'
 import 'stories/Input'
+
+addDecorator(story => {
+    <div style={{fontFamily: 'Roboto, sans-serif;'}}>
+        {story()}
+    </div>
+})
